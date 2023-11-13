59202a79f8761b4af90067c86059096fd8be20c6
Santiago Mejia Fernandez <santiagomejiaf@javeriana.edu.co>
2023-11-13 00:56:11 -0500

    Arreglo para usar Railway

--- src/main/java/pds/patronProxy/configuration/ConfiguracionDB.java
+++ src/main/java/pds/patronProxy/configuration/ConfiguracionDB.java
@@ -5,4 +5,8 @@ public class ConfiguracionDB {
     public static final String DB_URL_CONEXION = "jdbc:postgresql://localhost:5432/PatronProxy";
     public static final String DB_USER = "postgres";
     public static final String DB_PASSWORD = "admin";
+    public static final String DB_HOST = "localhost";
+    public static final String DB_PORT = "5432";
+    public static final String DB_NAME = "PatronProxy";
+
 }
